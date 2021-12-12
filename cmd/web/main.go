9a282f41f0c8eacf5aa71cceac0d2205c8f9bfb4
urchmaney <kingsobino@gmail.com>
2021-12-12 09:43:06 +0100

    add: mysql database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,36 @@ package main
 import (
   "net/http"
   "log"
+  "database/sql"
+  "flag"
+  _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+  dsn := flag.String("dsn", "root:root@tcp(db)/b_rec?parseTime=true", "MySQL data source name")
+  flag.Parse()
+
+  _, err := OpenDB(*dsn)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
   
   log.Println("starting server at port :3000")
-  err := http.ListenAndServe(":3000", mux)
+  err = http.ListenAndServe(":3000", mux)
   log.Println(err)
+}
+
+func OpenDB(dsn string)(*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+    return nil, err
+  }
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
 }
